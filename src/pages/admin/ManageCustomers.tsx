e8a27874a379b74d7604175228dbb2c14392d2da
Karina Allard <karina.allard.92@gmail.com>
2025-03-13 11:58:13 +0100

    Add some styling changes

--- src/pages/admin/ManageCustomers.tsx
+++ src/pages/admin/ManageCustomers.tsx
@@ -21,37 +21,52 @@ export const ManageCustomers = () => {
 			{error && <p>Error: {error}</p>}
 
 			<div className="customer-wrapper">
-                <Button type="button" className="edit-btn"><Link to={"/admin/create-customer"}>Create Customer</Link></Button>
+				<div className="button-div">
+					<Button type="button" className="edit-btn">
+						<Link to={"/admin/create-customer"}>Create Customer</Link>
+					</Button>
+				</div>
 				{customers.length === 0 && !isLoading && <p>No customers found.</p>}
 				{customers.map((customer) => (
 					<div className="customer-div" key={customer.id}>
 						<h3 onClick={() => showCustomerDetails(customer.id!)}>
 							{customer.firstname} {customer.lastname}
-                            {showCustomerByID === customer.id ? (
-                                <MdExpandLess />
-                                ) : ( 
-                                <MdExpandMore /> 
-                            )}
+							{showCustomerByID === customer.id ? (
+								<MdExpandLess />
+							) : (
+								<MdExpandMore />
+							)}
 						</h3>
 						{showCustomerByID === customer.id && (
 							<>
 								<div className="contact-info">
 									<h4>Email: </h4>
-                                    <p>{customer.email}</p>
-                                    <h4>Phone: </h4>
+									<p>{customer.email}</p>
+									<h4>Phone: </h4>
 									<p>{customer.phone}</p>
 								</div>
 								<div className="address-info">
-                                    <h4>Address: </h4>
+									<h4>Address: </h4>
 									<p>
 										{customer.street_address}, {customer.postal_code},{" "}
 										{customer.city}, {customer.country}
 									</p>
 								</div>
-                                <div className="button-div">
-                                    <Button className="edit-btn"><Link to={`/admin/update-customer/${customer.id}`}>Update</Link></Button>
-                                    <Button className="delete-btn" onClick={() => customer.id && deleteCustomerHandler(customer.id)}>Delete</Button>
-                                </div>
+								<div className="button-div">
+									<Button className="edit-btn">
+										<Link to={`/admin/update-customer/${customer.id}`}>
+											Update
+										</Link>
+									</Button>
+									<Button
+										className="delete-btn"
+										onClick={() =>
+											customer.id && deleteCustomerHandler(customer.id)
+										}
+									>
+										Delete
+									</Button>
+								</div>
 							</>
 						)}
 					</div>
--- src/pages/admin/UpdateCustomer.tsx
+++ src/pages/admin/UpdateCustomer.tsx
@@ -1,142 +1,145 @@
-import { useNavigate, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import { useCustomers } from "../hooks/useCustomers";
 import { useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { ICustomer } from "../../types/ICustomer";
 
 export const UpdateCustomer = () => {
-    const { customers, updateCustomerHandler } = useCustomers();
-    const { id } = useParams<{ id: string }>();
-    const navigate = useNavigate();
+	const { customers, updateCustomerHandler } = useCustomers();
+	const { id } = useParams<{ id: string }>();
+	const navigate = useNavigate();
 
-    const customer = customers.find((c) => c.id === Number(id))
+	const customer = customers.find((c) => c.id === Number(id));
 
-    const [formData, setFormData] = useState<ICustomer>({
-            firstname: "",
-            lastname: "",
-            email: "",
-            password: "",
-            phone: "",
-            street_address: "",
-            postal_code: "",
-            city: "",
-            country: "",
-        });
+	const [formData, setFormData] = useState<ICustomer>({
+		firstname: "",
+		lastname: "",
+		email: "",
+		password: "",
+		phone: "",
+		street_address: "",
+		postal_code: "",
+		city: "",
+		country: "",
+	});
 
-        useEffect(() => {
-            if (customer) {
-                setFormData({
-                    firstname: customer.firstname,
-                    lastname: customer.lastname,
-                    email: customer.email,
-                    password: customer.password,
-                    phone: customer.phone,
-                    street_address: customer.street_address,
-                    postal_code: customer.postal_code,
-                    city: customer.city,
-                    country: customer.country,
-                })
-            }
-        }, [customer])
+	useEffect(() => {
+		if (customer) {
+			setFormData({
+				firstname: customer.firstname,
+				lastname: customer.lastname,
+				email: customer.email,
+				password: customer.password,
+				phone: customer.phone,
+				street_address: customer.street_address,
+				postal_code: customer.postal_code,
+				city: customer.city,
+				country: customer.country,
+			});
+		}
+	}, [customer]);
 
-        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-            setFormData({
-                ...formData,
-                [e.target.name]: e.target.value,
-            });
-        };
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-        const handleSubmit = async (e: React.FormEvent) => {
-            e.preventDefault();
-            if (!customer) return;
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		if (!customer) return;
 
-            await updateCustomerHandler(customer.id as number, formData);
-            navigate("/admin/manage-customers")
-        }
+		await updateCustomerHandler(customer.id as number, formData);
+		navigate("/admin/manage-customers");
+	};
 
-        if (!customer) {
-            return <p>Customer not found.</p>
-        }
+	if (!customer) {
+		return <p>Customer not found.</p>;
+	}
 
-    return (
-            <div className="customer-wrapper">
-                <form onSubmit={handleSubmit}>
-                    <h2>Update Customer</h2>
-                    <div className="form-div">
-                        <input
-                            type="text"
-                            name="firstname"
-                            value={formData.firstname}
-                            onChange={handleChange}
-                            placeholder="First Name"
-                            required
-                        />
-                        <input
-                            type="text"
-                            name="lastname"
-                            value={formData.lastname}
-                            onChange={handleChange}
-                            placeholder="Last Name"
-                            required
-                        />
-                        <input
-                            type="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            placeholder="Email"
-                            required
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            placeholder="Password"
-                            className="input-pw"
-                            required
-                        />
-                        <input
-                            type="text"
-                            name="phone"
-                            value={formData.phone}
-                            onChange={handleChange}
-                            placeholder="Phone Number"
-                        />
-                        <input
-                            type="text"
-                            name="street_address"
-                            value={formData.street_address}
-                            onChange={handleChange}
-                            placeholder="Street Address"
-                        />
-                        <input
-                            type="text"
-                            name="postal_code"
-                            value={formData.postal_code}
-                            onChange={handleChange}
-                            placeholder="Postal Code"
-                        />
-                        <input
-                            type="text"
-                            name="city"
-                            value={formData.city}
-                            onChange={handleChange}
-                            placeholder="City"
-                        />
-                        <input
-                            type="text"
-                            name="country"
-                            value={formData.country}
-                            onChange={handleChange}
-                            placeholder="Country"
-                        />
-                    </div>
-    
-                    <Button type="submit" className="submit-btn">
-                        Update Customer
-                    </Button>
-                </form>
-            </div>
-        );
-}
+	return (
+		<div className="customer-wrapper">
+			<form onSubmit={handleSubmit}>
+				<h2>Update Customer</h2>
+				<div className="form-div">
+					<input
+						type="text"
+						name="firstname"
+						value={formData.firstname}
+						onChange={handleChange}
+						placeholder="First Name"
+						required
+					/>
+					<input
+						type="text"
+						name="lastname"
+						value={formData.lastname}
+						onChange={handleChange}
+						placeholder="Last Name"
+						required
+					/>
+					<input
+						type="email"
+						name="email"
+						value={formData.email}
+						onChange={handleChange}
+						placeholder="Email"
+						required
+					/>
+					<input
+						type="password"
+						name="password"
+						value={formData.password}
+						onChange={handleChange}
+						placeholder="Password"
+						className="input-pw"
+						required
+					/>
+					<input
+						type="text"
+						name="phone"
+						value={formData.phone}
+						onChange={handleChange}
+						placeholder="Phone Number"
+					/>
+					<input
+						type="text"
+						name="street_address"
+						value={formData.street_address}
+						onChange={handleChange}
+						placeholder="Street Address"
+					/>
+					<input
+						type="text"
+						name="postal_code"
+						value={formData.postal_code}
+						onChange={handleChange}
+						placeholder="Postal Code"
+					/>
+					<input
+						type="text"
+						name="city"
+						value={formData.city}
+						onChange={handleChange}
+						placeholder="City"
+					/>
+					<input
+						type="text"
+						name="country"
+						value={formData.country}
+						onChange={handleChange}
+						placeholder="Country"
+					/>
+				</div>
+
+				<div className="button-div">
+					<Button type="submit" className="submit-btn">
+						Update Customer
+					</Button>
+					<Link to={"/admin/manage-customers"}>Back</Link>
+				</div>
+			</form>
+		</div>
+	);
+};
