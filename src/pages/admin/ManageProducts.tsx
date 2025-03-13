2bea82955708cdb4d3e7d55f0f8526793d568783
Karina Allard <karina.allard.92@gmail.com>
2025-03-13 13:32:30 +0100

    Add create product button for easy navigation to create product page

--- src/pages/admin/ManageProducts.tsx
+++ src/pages/admin/ManageProducts.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router"
+import { Button } from "../../components/Button"
+
 export const ManageProducts = () => {
 
     return (
-        <>
+        <div className="product-wrapper">
+            <div className="button-div">
+					<Button type="button" className="edit-btn">
+						<Link to={"/admin/create-product"}>Create Product</Link>
+					</Button>
+				</div>
         
-        </>
+        </div>
     )
 }
