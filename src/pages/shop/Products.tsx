b33c1306d976e7f5ce637eb02a9dc98369b81cf1
Karina Allard <karina.allard.92@gmail.com>
2025-03-18 12:44:36 +0100

    Add product view and basic styling

--- src/pages/shop/Products.tsx
+++ src/pages/shop/Products.tsx
@@ -1,9 +1,32 @@
+import { Button } from "../../components/Button";
+import { useProducts } from "../hooks/useProducts"
+import "../../styles/shop.css"
+
 export const Products = () => {
+    const { products, isLoading, error } = useProducts();
 
     return (
 
-        <>
-            <h2>Hello this is the product page</h2>
-        </>
+        <div className="shop-wrapper">
+            {isLoading && <p>Loading products</p>}
+            {error && <p>Error: {error}</p>}
+            <div className="product-wrapper">
+                {products.length === 0 && !isLoading && <p>No products found.</p>}
+                {products.map((product) => (
+                    <div className="product-div" key={product.id}>
+                        <img src={product.image} alt={product.name} />
+                        <div className="product-text">
+                            <h4>{product.name}</h4>
+                            <p>{product.price} kr</p>                            
+                        </div>
+                        <div className="button-div">
+                            <Button className="cart-btn">Add To Cart</Button>
+                            <input type="number" />                            
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
