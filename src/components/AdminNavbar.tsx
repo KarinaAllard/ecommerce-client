14729c43862e888aaf6f2325929ae7f29c66d9b9
Karina Allard <karina.allard.92@gmail.com>
2025-03-28 12:01:24 +0100

    Add hamburger menu to Admin Nav as well

--- src/components/AdminNavbar.tsx
+++ src/components/AdminNavbar.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import { NavLink } from "react-router"
+import { FaBars, FaTimes } from "../icons";
 
 export const AdminNavbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+        const toggleMenu = () => {
+            setMenuOpen((isOpen) => !isOpen);
+        };
 
     return (
 
         <div className="nav-div">
-            <ul className="nav-bar">
-                <li className="nav-item"><NavLink to={"admin/manage-orders"}>Orders</NavLink></li>
-                <li className="nav-item"><NavLink to={"admin/manage-customers"}>Customers</NavLink></li>
-                <li className="nav-item"><NavLink to={"admin/manage-products"}>Products</NavLink></li>
-                <li className="nav-item"><NavLink to={"/products"}>Exit Admin</NavLink></li>
+             <div className="menu-icon" onClick={toggleMenu}>
+                            {menuOpen ? <FaTimes /> : <FaBars />}
+                        </div>
+            <ul className={`nav-bar ${menuOpen ? "open" : ""}`}>
+                <li className="nav-item">
+                    <NavLink to={"admin/manage-orders"} onClick={() => setMenuOpen(false)}>Orders</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to={"admin/manage-customers"} onClick={() => setMenuOpen(false)}>Customers</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to={"admin/manage-products"} onClick={() => setMenuOpen(false)}>Products</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to={"/products"} onClick={() => setMenuOpen(false)}>Exit Admin</NavLink>
+                </li>
             </ul>
         </div>
     )
