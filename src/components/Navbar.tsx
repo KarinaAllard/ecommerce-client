b924b0bfb952cf8135100ea16b7cc9e855968175
Karina Allard <karina.allard.92@gmail.com>
2025-03-28 11:55:30 +0100

    Add hamburger menu to the nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,36 @@
-import { FaShoppingCart } from "react-icons/fa"
+import { useState } from "react"
+import { FaShoppingCart, FaTimes, FaBars } from "../icons"
 import { NavLink } from "react-router"
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen((isOpen) => !isOpen);
+    };
 
     return (
 
         <div className="nav-div">
-            <ul className="nav-bar">
-                <li className="nav-item"><NavLink to={"/"}>Home</NavLink></li>
-                <li className="nav-item"><NavLink to={"/products"}>Shop</NavLink></li>
-                <li className="nav-item"><NavLink to={"/admin/manage-products"}>Admin</NavLink></li>    
+
+            <div className="menu-icon" onClick={toggleMenu}>
+                {menuOpen ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={`nav-bar ${menuOpen ? "open" : ""}`}>
+                <li className="nav-item">
+                    <NavLink to={"/"} onClick={() => setMenuOpen(false)}>Home</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to={"/products"} onClick={() => setMenuOpen(false)}>Shop</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to={"/admin/manage-products"} onClick={() => setMenuOpen(false)}>Admin</NavLink>
+                </li>    
             </ul>
             <div className="cart-icon">
-                <NavLink to={"/cart"}><FaShoppingCart/></NavLink>
+                <NavLink to={"/cart"}>
+                    <FaShoppingCart/>
+                </NavLink>
             </div>
         </div>
     )
